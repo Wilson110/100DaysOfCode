8314fe4e3829f9a9f9f8b1b7360a0e830bbbdfd7
SheThrives11 <ewilson110@gmail.com>
2016-02-13 22:54:31 -0600

    created albums.js with gallery data

---
+++ LondonURWD/photo_gallery/js/albums.js
@@ -0,0 +1,75 @@
+// data to be displayed in templates using gallery.js code
+
+var gallery = {
+	albums : [
+		{
+			name : "Planets", 
+			thumbnail : "images/img_1.jpg",
+			photos : [
+				{	
+					src : "images/img_1.jpg",
+					title : "Earth", 
+					description : "View of the Mid-Pacific Ocean from Galileo"
+				}, 
+				{	
+					src : "images/img_6.jpg",
+					title : "Mars", 
+					description : "Late spring on Mars"
+				}, 
+				{	
+					src : "images/img_7.jpg",
+					title : "Jupiter", 
+					description : "Three-color filter of Jupiter"
+				}, 
+				{	
+					src : "images/img_8.jpg",
+					title : "Saturn", 
+					description : "Full color filter of Saturn and its rings"
+				} 
+			]
+		},
+		{
+			name : "Moons", 
+			thumbnail : "images/img_4.jpg",
+			photos : [
+				{	
+					src : "images/img_4.jpg",
+					title : "Iapetus", 
+					description : "Saturn moon founded in 1671 by Cassini"
+				}, 
+				{	
+					src : "images/img_9.jpg",
+					title : "Dione", 
+					description : "Saturn IV moon founded in 1684 by Cassini"
+				}, 
+				{	
+					src : "images/img_12.jpg",
+					title : "Titan", 
+					description : "Saturn VI moon founded in 1655 by Huygens"
+				}  
+			]
+		},
+		{
+			name : "Moonwalks", 
+			thumbnail : "images/img_17.jpg",
+			photos : [
+				{	
+					src : "images/img_16.jpg",
+					title : "Shephard Plants Flag", 
+					description : "From JSC Center. EVAs"
+				}, 
+				{	
+					src : "images/img_17.jpg",
+					title : "With bean samples", 
+					description : "From JSC Center. EVAs"
+				}, 
+				{	
+					src : "images/img_18.jpg",
+					title : "Scott gives salute", 
+					description : "From JSC Center. EVAs"
+				} 
+			]
+		}
+
+	]
+};

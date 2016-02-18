12ad2fca0babf3cf1332d35e68105158c56a7f27
SheThrives11 <ewilson110@gmail.com>
2016-02-17 21:01:12 -0600

    enabled #slideshow-tab function

--- LondonURWD/photo_gallery/js/gallery.js
+++ LondonURWD/photo_gallery/js/gallery.js
@@ -27,78 +27,13 @@ $(document).ready(function(){
 	source = $('#photos-template').html();
 	photos_template = Handlebars.compile(source);
 
-	source   = $("#modal-template").html();
-    var modal_template = Handlebars.compile(source);
-
 	source = $('#photo-template').html();
 	photo_template = Handlebars.compile(source);
 
 	source = $('#slideshow-template').html();
 	slideshow_template = Handlebars.compile(source);
 
-	//image data for search
-	var data = {images:[
-        {	
-			src : "images/img_1.jpg",
-			title : "Earth", 
-			description : "View of the Mid-Pacific Ocean from Galileo",
-			author: "NASA"
-		}, 
-		{	
-			src : "images/img_4.jpg",
-			title : "Iapetus", 
-			description : "Saturn moon founded in 1671 by Cassini",
-			author: "NASA"		
-		},  
-        {	
-			src : "images/img_6.jpg",
-			title : "Mars", 
-			description : "Late spring on Mars",
-			author: "NASA"
-		}, 
-		{	
-			src : "images/img_7.jpg",
-			title : "Jupiter", 
-			description : "Three-color filter of Jupiter",
-			author: "NASA"	
-		}, 
-		{	
-			src : "images/img_8.jpg",
-			title : "Saturn", 
-			description : "Full color filter of Saturn and its rings",
-			author: "NASA"		
-		}
-		{	
-			src : "images/img_9.jpg",
-			title : "Dione", 
-			description : "Saturn IV moon founded in 1684 by Cassini",
-			author: "NASA"		
-		}, 
-		{	
-			src : "images/img_12.jpg",
-			title : "Titan", 
-			description : "Saturn VI moon founded in 1655 by Huygens",
-			author: "NASA"
-		}
-		{	
-			src : "images/img_16.jpg",
-			title : "Shephard Plants Flag", 
-			description : "Moonwalk pic from JSC Center. EVAs",
-			author: "NASA"
-		}, 
-		{	
-			src : "images/img_17.jpg",
-			title : "With bean samples", 
-			description : "Moonwalk pic from JSC Center. EVAs",
-			author: "NASA"
-		}, 
-		{	
-			src : "images/img_18.jpg",
-			title : "Scott gives salute", 
-			description : "Moonwalk pic from JSC Center. EVAs",
-			author: "NASA"
-		}   
-    ]};
+	
 
 	//Step 6: Create a click function for #albums-tab from the HTML
 	$("#albums-tab").click(function(){
@@ -158,51 +93,5 @@ $(document).ready(function(){
 
 	// start the page by showing the albums view upon click
 	$("#albums-tab").click();
-
-	// Modal function 
-	 function displayModal(event){
-        var imageNumber = $(this).data("id");
-        // get the image out of the array using the image
-        // number and render it using the modal template
-        var html    = modal_template(data.images[imageNumber]);
-        // put the modal template in the DOM
-        $('#modal-container').html(html);
-        // show the modal
-        $("#imageModal").modal('show');
-    }
-    // display the modal when you click on a thumbnail
-    $('.thumbnail').click(displayModal);
-
-    // Search function
-    $('#searchbox').keypress(function (e) {
-      // the return key has an id of 13, if it's pressed...
-      if (e.which == 13) {
-        // get the search text which is the contents of the search box
-        var search_text = $('#searchbox').val();
-        // print the search box (for debugging)
-        console.log(search_text)
-        // create a new array of data with only the data that contains the search string
-        var filteredData = {
-          images: data.images.filter(function(d){
-            // return true if the title contains the search text
-            if (d.title.search(search_text) > -1){
-              return true;
-            }
-            // return true if the author contains the search text
-            if (d.author.search(search_text) > -1){
-              return true;
-            }
-            // if we reach here it means we haven't found a match so return false
-            return false;
-          })
-        };
-        
-        // pass the newly filtered data into
-        // the template to generate new html
-        var html    = template(filteredData);
-        $('#content').html(html);
-
-        // display the modal when you click on a thumbnail
-        $('.thumbnail').click(displayModal);
-      }
+	
 });

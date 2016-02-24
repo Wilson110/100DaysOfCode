9e98b88e165fdf9c21612dfb6c7a14cbe74c20b4
SheThrives11 <ewilson110@gmail.com>
2016-02-23 20:46:56 -0600

    added data to the object inner arrays

--- Templating/js/data.js
+++ Templating/js/data.js
@@ -39,43 +39,43 @@ var city_data = {
 				},
 				{
 					src :
-					title : "7"
-					description : 
+					title : "7. Palm-Bay-Melbourne-Titusville, FL"
+					description : "Advanced industries, share of employment: 13.4%. Share of output: 21.7%"
 				},
 				{
 					src :
-					title : "8"
-					description : 
+					title : "8. Boston-Cambridge-Newton, MA-NH",
+					description : "Advanced industries, share of employment: 13.3%. Share of output: 22.8%"
 				},
 				{
 					src :
-					title : "9"
-					description : 
+					title : "9. Houston-The Woodlands-Sugar Land, TX",
+					description : "Advanced industries, share of employment: 12.8%. Share of output: 38.4%"
 				},
 				{
 					src :
-					title : "10"
-					description : 
+					title : "10. San Diego-Carlsbad, CA",
+					description : "Advanced industries, share of employment: 12.3%. Share of output: 21.2%"
 				},
 				{
 					src :
-					title : "11"
-					description : 
+					title : "11. Austin-Round Rock, TX",
+					description : "Advanced industries, share of employment: 12.1%. Share of output: 24.9%"
 				},
 				{
 					src :
-					title : "12"
-					description : 
+					title : "12. Provo-Orem, UT",
+					description : "Advanced industries, share of employment: 12.0%. Share of output: 23.2%"
 				},
 				{
 					src :
-					title : "13. Raleigh, NC"
-					description : "Advnaced industries,"
+					title : "13. Raleigh, NC",
+					description : "Advnaced industries, share of employment: 11.7%. Share of output: 23.4%"
 				},
 				{
 					src :
 					title : "14. Ogden-Clearfield, UT",
-					description : "Advanced industries, share of employment: 11.3%. Advanced industries, share of output: 20.5%"
+					description : "Advanced industries, share of employment: 11.3%. Share of output: 20.5%"
 				},
 				{
 					src :

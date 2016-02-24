2b89e57ab1aaabf6975963268869851c53f6cae1
SheThrives11 <ewilson110@gmail.com>
2016-02-23 20:29:29 -0600

    created data.js that will feed template content

---
+++ Templating/js/data.js
@@ -0,0 +1,88 @@
+//this file contains the data needed for the templates
+// 15 cities with the most high-tech jobs
+
+var city_data = {
+	city : [
+		{
+			name : "The South",
+			thumbnail :
+			info : [
+				{
+					src :
+					title : "1"
+					description : 
+				},
+				{
+					src :
+					title : "2"
+					description : 
+				},
+				{
+					src :
+					title : "3"
+					description : 
+				},
+				{
+					src :
+					title : "4"
+					description : 
+				},
+				{
+					src :
+					title : "5"
+					description : 
+				},
+				{
+					src :
+					title : "6"
+					description : 
+				},
+				{
+					src :
+					title : "7"
+					description : 
+				},
+				{
+					src :
+					title : "8"
+					description : 
+				},
+				{
+					src :
+					title : "9"
+					description : 
+				},
+				{
+					src :
+					title : "10"
+					description : 
+				},
+				{
+					src :
+					title : "11"
+					description : 
+				},
+				{
+					src :
+					title : "12"
+					description : 
+				},
+				{
+					src :
+					title : "13. Raleigh, NC"
+					description : "Advnaced industries,"
+				},
+				{
+					src :
+					title : "14. Ogden-Clearfield, UT",
+					description : "Advanced industries, share of employment: 11.3%. Advanced industries, share of output: 20.5%"
+				},
+				{
+					src :
+					title : "15. Salt Lake City, UT",
+					description : "Advanced industries, share of employment: 11.1%. Advanced industries, share of output: 16.5%"
+				}
+			]
+		}
+	]
+};

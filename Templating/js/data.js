7b5ebd98091eb95d5890b18e62120e2d6d7b0f17
SheThrives11 <ewilson110@gmail.com>
2016-02-24 22:46:25 -0600

    sectioned data using an object-array format, debugged data.js

--- Templating/js/data.js
+++ Templating/js/data.js
@@ -5,7 +5,7 @@
 var city_data = {
 	city : [
 		{
-			name : "(Add area)",
+			region : "West",
 			thumbnail :
 			info : [
 				{
@@ -15,43 +15,8 @@ var city_data = {
 				},
 				{
 					src :
-					title : "2. Seattle-Tacoma-Bellevue, WA",
-					description : "Advanced industries, share of employment: 16.0%. Share of output: 32.6%" 
-				},
-				{
-					src :
-					title : "3. Wichita, KS",
-					description : "Advanced industries, share of employment: 15.5%. Share of output: 27.4%"
-				},
-				{
-					src :
-					title : "4. Detroit-Warren-Dearboarn, MI",
-					description : "Advanced industries, share of employment: 14.8%. Share of output: 24.8%"
-				},
-				{
-					src :
 					title : "5. San Francisco-Oakland-Hayward, CA",
 					description : "Advanced industries, share of employment: 14.0%. Share of output: 24.7%"
-				},
-				{
-					src :
-					title : "6. Washington-Arlington-Alexandria, DMV",
-					description : "Advanced industries, share of employment: 13.7%. Share of output: 19.9%"
-				},
-				{
-					src :
-					title : "7. Palm-Bay-Melbourne-Titusville, FL",
-					description : "Advanced industries, share of employment: 13.4%. Share of output: 21.7%"
-				},
-				{
-					src :
-					title : "8. Boston-Cambridge-Newton, MA-NH",
-					description : "Advanced industries, share of employment: 13.3%. Share of output: 22.8%"
-				},
-				{
-					src :
-					title : "9. Houston-The Woodlands-Sugar Land, TX",
-					description : "Advanced industries, share of employment: 12.8%. Share of output: 38.4%"
 				},
 				{
 					src :
@@ -60,18 +25,8 @@ var city_data = {
 				},
 				{
 					src :
-					title : "11. Austin-Round Rock, TX",
-					description : "Advanced industries, share of employment: 12.1%. Share of output: 24.9%"
-				},
-				{
-					src :
 					title : "12. Provo-Orem, UT",
 					description : "Advanced industries, share of employment: 12.0%. Share of output: 23.2%"
-				},
-				{
-					src :
-					title : "13. Raleigh, NC",
-					description : "Advnaced industries, share of employment: 11.7%. Share of output: 23.4%"
 				},
 				{
 					src :
@@ -84,6 +39,83 @@ var city_data = {
 					description : "Advanced industries, share of employment: 11.1%. Advanced industries, share of output: 16.5%"
 				}
 			]
-		}
+		},
+		{
+			region : "Midwest",
+			thumbnail : 
+			info : [
+				{
+					src :
+					title : "3. Wichita, KS",
+					description : "Advanced industries, share of employment: 15.5%. Share of output: 27.4%"
+				},
+				{
+					src :
+					title : "4. Detroit-Warren-Dearboarn, MI",
+					description : "Advanced industries, share of employment: 14.8%. Share of output: 24.8%"
+				}
+			]
+		},
+		{
+			region : "East Coast",
+			thumbnail :
+			info : [
+				{
+					src :
+					title : "8. Boston-Cambridge-Newton, MA-NH",
+					description : "Advanced industries, share of employment: 13.3%. Share of output: 22.8%"
+				},
+			]
+		},
+		{
+			region : "Northwest",
+			thumbnail : 
+			info : [
+				{
+					src :
+					title : "2. Seattle-Tacoma-Bellevue, WA",
+					description : "Advanced industries, share of employment: 16.0%. Share of output: 32.6%" 
+				}
+			]
+		},
+		{
+			region : "Appalachia",
+			thumbnail :
+			info : [
+				{
+					src :
+					title : "6. Washington-Arlington-Alexandria, DMV",
+					description : "Advanced industries, share of employment: 13.7%. Share of output: 19.9%"
+				},
+				{
+					src :
+					title : "13. Raleigh, NC",
+					description : "Advnaced industries, share of employment: 11.7%. Share of output: 23.4%"
+				}
+			]
+		},
+		{
+			region : "South",
+			thumbnail : 
+			info : [
+				{
+					src :
+					title : "7. Palm-Bay-Melbourne-Titusville, FL",
+					description : "Advanced industries, share of employment: 13.4%. Share of output: 21.7%"
+				},
+				
+				{
+					src :
+					title : "9. Houston-The Woodlands-Sugar Land, TX",
+					description : "Advanced industries, share of employment: 12.8%. Share of output: 38.4%"
+				},
+				
+				{
+					src :
+					title : "11. Austin-Round Rock, TX",
+					description : "Advanced industries, share of employment: 12.1%. Share of output: 24.9%"
+				}
+			]
+		}			
 	]
 };

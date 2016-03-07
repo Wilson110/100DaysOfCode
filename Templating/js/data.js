d37be422e11b4312c11f9c295e339ba6a0a1e4ac
SheThrives11 <ewilson110@gmail.com>
2016-03-06 19:59:05 -0600

    updated data in cities.description property

--- Templating/js/data.js
+++ Templating/js/data.js
@@ -16,27 +16,27 @@ var city_data = {
 				{
 					src : "images/sanfrancisco.jpg",
 					title : "5. San Francisco-Oakland-Hayward, CA",
-					description : "Advanced industries, share of employment: 14.0%. Share of output: 24.7%"
+					description : "Advanced industries, share of employment: 14.0%. Advanced industries, share of output: 24.7%. Annual average wage: $80,960. Largest advanced industry: Computer Systems Design."
 				},
 				{
 					src : "images/sandiego.jpg",
 					title : "10. San Diego-Carlsbad, CA",
-					description : "Advanced industries, share of employment: 12.3%. Share of output: 21.2%"
+					description : "Advanced industries, share of employment: 12.3%. Advanced industries, share of output: 21.2%. Annual average wage: $58,850. Largest advanced inudstry: Scientific Research and Data."
 				},
 				{
 					src : "images/provo.jpg",
 					title : "12. Provo-Orem, UT",
-					description : "Advanced industries, share of employment: 12.0%. Share of output: 23.2%"
+					description : "Advanced industries, share of employment: 12.0%. Advanced industries, share of output: 23.2%. Annual average wage: $39,940. Largest advanced industry: Computer Systems Design."
 				},
 				{
 					src : "images/ogden.jpg",
 					title : "14. Ogden-Clearfield, UT",
-					description : "Advanced industries, share of employment: 11.3%. Share of output: 20.5%"
+					description : "Advanced industries, share of employment: 11.3%. Advanced industries, share of output: 20.5%. Annual average wage: $40,180. Largest advanced industry: Motor Vehicle Parts Manufacturing."
 				},
 				{
 					src : "images/slc.jpg",
 					title : "15. Salt Lake City, UT",
-					description : "Advanced industries, share of employment: 11.1%. Advanced industries, share of output: 16.5%"
+					description : "Advanced industries, share of employment: 11.1%. Advanced industries, share of output: 16.5%. Annual average wage: $48,780. Largest advanced industry: Computer Systems Design."
 				}
 			]
 		},
@@ -47,12 +47,12 @@ var city_data = {
 				{
 					src : "images/wichita.jpg",
 					title : "3. Wichita, KS",
-					description : "Advanced industries, share of employment: 15.5%. Share of output: 27.4%"
+					description : "Advanced industries, share of employment: 15.5%. Advanced industries, share of output: 27.4%. Annual average wage: $44,410. Largest advanced industry: Aerospace Product and Parts Manufacturing."
 				},
 				{
 					src : "images/detroit.jpg",
 					title : "4. Detroit-Warren-Dearboarn, MI",
-					description : "Advanced industries, share of employment: 14.8%. Share of output: 24.8%"
+					description : "Advanced industries, share of employment: 14.8%. Advanced industries, share of output: 24.8%. Annual average wage: $53,300. Largest advanced industry: Motor Vehicle Parts Manufacturing."
 				}
 			]
 		},
@@ -63,7 +63,7 @@ var city_data = {
 				{
 					src : "images/boston.jpg",
 					title : "8. Boston-Cambridge-Newton, MA-NH",
-					description : "Advanced industries, share of employment: 13.3%. Share of output: 22.8%"
+					description : "Advanced industries, share of employment: 13.3%. Advanced industries, share of output: 22.8%. Annual average wage: $67,370. Largest advanced industry: Computer Systems Design."
 				},
 			]
 		},
@@ -74,7 +74,7 @@ var city_data = {
 				{
 					src : "images/seattle.jpg",
 					title : "2. Seattle-Tacoma-Bellevue, WA",
-					description : "Advanced industries, share of employment: 16.0%. Share of output: 32.6%" 
+					description : "Advanced industries, share of employment: 16.0%. Advanced industries, share of output: 32.6%. Annual average wage: $63,180. Largest advanced industry: Aerospace Product and Parts Manufacturing." 
 				}
 			]
 		},
@@ -85,12 +85,12 @@ var city_data = {
 				{
 					src : "images/dc.jpg",
 					title : "6. Washington-Arlington-Alexandria, DMV",
-					description : "Advanced industries, share of employment: 13.7%. Share of output: 19.9%"
+					description : "Advanced industries, share of employment: 13.7%. Advanced industries, share of output: 19.9%. Annual average wage: $68,370. Largest advanced industry: Computer Systems Design."
 				},
 				{
 					src : "images/raleigh.jpg",
 					title : "13. Raleigh, NC",
-					description : "Advnaced industries, share of employment: 11.7%. Share of output: 23.4%"
+					description : "Advanced industries, share of employment: 11.7%. Advanced industries, share of output: 23.4%. Annual average wage: $51,630. Largest advanced industry: Computer Systems Design."
 				}
 			]
 		},
@@ -101,19 +101,19 @@ var city_data = {
 				{
 					src : "images/palmbay.jpg",
 					title : "7. Palm-Bay-Melbourne-Titusville, FL",
-					description : "Advanced industries, share of employment: 13.4%. Share of output: 21.7%"
+					description : "Advanced industries, share of employment: 13.4%. Advanced industries, share of output: 21.7%. Annual average wage: $47,350. Largest advanced industry: Audio and Video Equipment Manufacturing."
 				},
 				
 				{
 					src : "images/houston.jpg",
 					title : "9. Houston-The Woodlands-Sugar Land, TX",
-					description : "Advanced industries, share of employment: 12.8%. Share of output: 38.4%"
+					description : "Advanced industries, share of employment: 12.8%. Advanced industries, share of output: 38.4%. Annual average wage: $63,880. Largest advanced industry: Architectural, Engineering, and Related Services."
 				},
 				
 				{
 					src : "images/austin.jpg",
 					title : "11. Austin-Round Rock, TX",
-					description : "Advanced industries, share of employment: 12.1%. Share of output: 24.9%"
+					description : "Advanced industries, share of employment: 12.1%. Advanced industries, share of output: 24.9%. Annual average wage: $53,510. Largest advanced industry: Computer Systems Design."
 				}
 			]
 		}			

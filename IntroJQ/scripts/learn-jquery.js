ec38cc9a60c2667c100b219b5ed4e8926919a9cb
SheThrives11 <ewilson110@gmail.com>
2016-04-10 17:45:32 -0500

    added some test demo code, commented out when not in use

---
+++ IntroJQ/scripts/learn-jquery.js
@@ -0,0 +1,18 @@
+$(document).ready(function(){
+	//BASIC SELECTORS
+
+	//BASIC ANIMATIONS
+	//$('.box:first').slideUp(500).slideDown(800);
+
+	//$('.box:second').animate({bottom: '200px', left: '200px',}, 800);
+
+	//INDEX FILTERS
+	//$('p:gt(2)').css('border', '4px solid red');
+
+	//RELATIONSHIP FILTERS
+	//$('h2:has(span)').css('color', 'blue');
+	//$('.box:empty').css('border', '4px solid grey');
+
+	//ATTRIBUTE FILTERS
+	//$('p[name="shorty"]').css('background-color', 'yellow');
+});

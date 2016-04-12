f9e1342d506186b1eff0168066b9dd08596fa6e4
SheThrives11 <ewilson110@gmail.com>
2016-04-11 20:32:18 -0500

    added attribute, class, and text method examples to jquery test file

--- IntroJQ/scripts/learn-jquery.js
+++ IntroJQ/scripts/learn-jquery.js
@@ -15,4 +15,19 @@ $(document).ready(function(){
 
 	//ATTRIBUTE FILTERS
 	//$('p[name="shorty"]').css('background-color', 'yellow');
+
+	//ATTRIBUTE METHODS
+	//$('p:first').attr('class', 'not-lead');
+	//$('img').attr("src", "img2.jpg");
+	$('img').delay(400).fadeOut(500, function(){
+		$(this).attr('src', 'img2.jpg').fadeIn(500);
+	})
+
+	//CLASS METHODS
+	//$('p').toggleClass('blue').removeClass('lead');
+
+	//CONTENT METHODS
+	$('p:first').text('Hey you guyyyysss!');
+	//alert($('p:first').text());
+	$('input').val('yo dude!');
 });

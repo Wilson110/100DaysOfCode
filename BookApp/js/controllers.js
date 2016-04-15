1231588add6aee4fa5e3aa2b1f2b1a068870a39b
SheThrives11 <ewilson110@gmail.com>
2016-04-14 20:20:43 -0500

    added plusOne() and minusOne() functions to $scope

--- BookApp/js/controllers.js
+++ BookApp/js/controllers.js
@@ -6,37 +6,55 @@ app.controller('MainController', ['$scope', function($scope){
 			name: "The 4-Hour Body",
 			price: 39,
 			pubdate: new Date('2014', '12', '14'),
-			cover: 'img/4-hour.jpg'
+			cover: 'img/4-hour.jpg',
+			likes: 0,
+			dislikes: 0
 		},
 		{
 			name: "Scope & Closures",
 			price: 11,
 			pubdate: new Date('2008', '05', '01'),
-			cover: 'img/know-js.jpg'
+			cover: 'img/know-js.jpg',
+			likes: 0,
+			dislikes: 0
 		},
 		{
 			name: "The Hard Things About Hard Things",
 			price: 21,
 			pubdate: new Date('2014', '03', '04'),
-			cover: 'img/hard-things.jpg'
+			cover: 'img/hard-things.jpg',
+			likes: 0,
+			dislikes: 0
 		},
 		{
 			name: "HTML and CSS",
 			price: 19,
 			pubdate: new Date('2011', '10', '25'),
-			cover: 'img/html-and-css.jpg'
+			cover: 'img/html-and-css.jpg',
+			likes: 0,
+			dislikes: 0
 		},
 		{
 			name: "The Paleo Manifesto",
 			price: 12,
 			pubdate: new Date('2014', '05', '20'),
-			cover: 'img/paleo.jpg'
+			cover: 'img/paleo.jpg',
+			likes: 0,
+			dislikes: 0
 		},
 		{
 			name: "Zero To One",
 			price: 16,
 			pubdate: new Date('2014', '09', '16'),
-			cover: 'img/zero-to-one.jpg'
+			cover: 'img/zero-to-one.jpg',
+			likes: 0,
+			dislikes: 0
 		}
 	];
-}])
+	$scope.plusOne = function(index) {
+		$scope.products[index].likes += 1;
+	};
+	$scope.minusOne = function(index) {
+		$scope.products[index].dislikes += 1;
+	}
+}]);

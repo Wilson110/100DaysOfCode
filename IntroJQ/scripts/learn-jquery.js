c4706f88132576b64f87410ac75a0ab1e1daef25
SheThrives11 <ewilson110@gmail.com>
2016-04-18 19:38:49 -0500

    added DOM tranversal and event binding method examples

--- IntroJQ/scripts/learn-jquery.js
+++ IntroJQ/scripts/learn-jquery.js
@@ -19,15 +19,31 @@ $(document).ready(function(){
 	//ATTRIBUTE METHODS
 	//$('p:first').attr('class', 'not-lead');
 	//$('img').attr("src", "img2.jpg");
-	$('img').delay(400).fadeOut(500, function(){
+	/* $('img').delay(400).fadeOut(500, function(){
 		$(this).attr('src', 'img2.jpg').fadeIn(500);
-	})
+	}) */
 
 	//CLASS METHODS
 	//$('p').toggleClass('blue').removeClass('lead');
 
 	//CONTENT METHODS
-	$('p:first').text('Hey you guyyyysss!');
+	//$('p:first').text('Hey you guyyyysss!');
 	//alert($('p:first').text());
-	$('input').val('yo dude!');
+	//$('input').val('yo dude!');
+
+	//DOM Traversal
+	//$('h2').parents().css('border', '4px solid red');
+	//$('h2').sibliling('p:first').css('border', '4px solid red');
+	//$('h2').parents('section').siblings('header').css('border', '4px solid red');
+	//$('h2').parents('section').siblings('header').children().css('border', '4px solid red');
+
+	//EVENT BINDING
+	/* $('h2').click(function(){
+		$(this).toggleClass('blue');
+	}); */
+
+	$('html').keypress(function(){
+		$(this).toggleClass('blue');
+	});
+
 });

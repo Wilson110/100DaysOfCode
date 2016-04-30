17132f47c18a44795320f98d60c08d94175e5d19
SheThrives11 <ewilson110@gmail.com>
2016-04-29 22:10:21 -0500

    Switched out Angular router code with ui-router code.
    added states for / and /:id

--- BookApp/js/app.js
+++ BookApp/js/app.js
@@ -1,1 +1,34 @@
-var app = angular.module("myApp", []);
+'use strict';
+
+angular.module('myApp', ['ui.router'])
+.config(['$stateProvider', function($stateProvider){
+	$stateProvider
+
+		.state('app', {
+			url:'/',
+			views: {
+				'header': {
+					templateUrl: 'views/header.html',
+					controller: 'IndexController'
+				},
+				'content': {
+					templateUrl: 'views/main.html',
+					controller: 'MainController'
+				},
+				'footer': {
+					templateUrl: 'views/footer.html',
+				}
+			}
+		})
+		.state('app.bookdetails', {
+			url: '/:id',
+			views: {
+				'content@': {
+					templateUrl: 'views/book.html',
+					controller: 'MainController'
+				}
+			}
+		});
+	$urlRouterProvider.otherwise('/');
+}]);
+
